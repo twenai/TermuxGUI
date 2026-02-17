85dbc2c5163b40f7dd1a07954309c343016bf1cb
twenai <twenaisup@gmail.com>
2026-02-18 02:53:42 +0800

    Update

---
+++ app/src/main/java/com/twen/xai/MainActivity.java
@@ -0,0 +1,18 @@
+
+package com.twen.xai;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.material.button.MaterialButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        MaterialButton button = new MaterialButton(this);
+        button.setText("TwenX Ready");
+        setContentView(button);
+    }
+}
